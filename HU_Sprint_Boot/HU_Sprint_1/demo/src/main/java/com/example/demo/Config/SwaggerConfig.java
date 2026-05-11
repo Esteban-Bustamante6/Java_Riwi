595a72a60ffec3b18d3a0164fff1e38fcbb0e2aa
Esteban-Bustamante6 <estebanbl090@gmail.com>
2026-05-11 08:19:26 -0500

    se agrego un readme detallado sobre que es el proyecto y como inicializarlo

--- HU_Sprint_Boot/HU_Sprint_1/demo/src/main/java/com/example/demo/Config/SwaggerConfig.java
+++ HU_Sprint_Boot/HU_Sprint_1/demo/src/main/java/com/example/demo/Config/SwaggerConfig.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Configuration;
         info = @Info(
                 title = "API of gestation of Corders",
                 version = "1.0",
-                summary = "Description" , description = "This is a Event and Venues management system where you can list and create new coders and assign them to a specific group (clan). You can also filter them by their clan or ID, as well as edit and delete them.",
+                summary = "Description" , description = "Eventify is a comprehensive management system developed for the Eventify company, designed to centralize and manage information about events and their venues. The system allows validating all necessary information before starting any event, ensuring that all details are properly configured and verified",
                 contact = @Contact(name = "Soporte EBL", email = "soporte@riwi.io")
         )
 )
